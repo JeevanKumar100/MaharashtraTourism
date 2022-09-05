4f697d83b63bc17dfc109029a0b9f548b6bc86e5
Omkar Jadhav <omkar28399@gmail.com>
2022-09-05 20:19:28 +0530

    some changes added to see if icons work

--- MaharashtraGadkille/settings.py
+++ MaharashtraGadkille/settings.py
@@ -145,6 +145,7 @@ AWS_S3_SIGNATURE_VERSION = "s3v4"
 AWS_S3_REGION_NAME = "ap-south-1"
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
+AWS_QUERYSTRING_AUTH = False
 DEFAULT_FILE_STORAGE = 'gadkille.storage_backends.PublicMediaStorage'
 STATICFILES_STORAGE = 'gadkille.storage_backends.StaticStorage'
 
