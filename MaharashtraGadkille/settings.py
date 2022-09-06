5e380f316747281d8f11c23925aadb6fe7280f6c
Omkar Jadhav <omkar28399@gmail.com>
2022-09-06 11:38:27 +0530

    storages added

--- MaharashtraGadkille/settings.py
+++ MaharashtraGadkille/settings.py
@@ -39,9 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'import_export',
-
-    'gadkille',
-    
+    'storages',
+    'gadkille',    
 ]
 
 MIDDLEWARE = [
