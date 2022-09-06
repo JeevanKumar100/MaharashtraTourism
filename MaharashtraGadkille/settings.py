e4b483a510c86e3de6cb26a989e22a83f649b7d2
Omkar Jadhav <omkar28399@gmail.com>
2022-09-06 10:04:47 +0530

    logging handler added

--- MaharashtraGadkille/settings.py
+++ MaharashtraGadkille/settings.py
@@ -160,6 +160,39 @@ STATICFILES_STORAGE = 'gadkille.storage_backends.StaticStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'mysite.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'MYAPP': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
 try:
     from .local_settings import *
 except ImportError:
