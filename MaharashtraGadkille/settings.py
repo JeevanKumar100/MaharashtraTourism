7642a21d18e44c3cc3e97a4a2b7d11218ad94f43
Mahesh Jadhav <omkar28399@gmail.com>
2022-09-06 20:36:51 +0530

    debug true

--- MaharashtraGadkille/settings.py
+++ MaharashtraGadkille/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3tn(sltkh$mz&#tth_)ik_uvx7vs4pt0jqvgm56x82dtsd4ut9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['maharashtragadkille.com','3.111.205.50','www.maharashtragadkille.com']
 
