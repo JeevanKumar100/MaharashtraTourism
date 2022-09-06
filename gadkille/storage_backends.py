4609ca7eb1ea0b4004ce38a6f4cf9635dbc1bbe7
Omkar Jadhav <omkar28399@gmail.com>
2022-09-06 09:29:14 +0530

    boto class changed and debug false

--- gadkille/storage_backends.py
+++ gadkille/storage_backends.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage,S3ManifestStaticStorage
 
 
-class StaticStorage(S3Boto3Storage):
+class StaticStorage(S3ManifestStaticStorage):
     location = 'static'
     file_overwrite = False
 
